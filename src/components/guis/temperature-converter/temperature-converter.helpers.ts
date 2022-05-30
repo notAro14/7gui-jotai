d1d8fd4a36460b7d0a960c4e8c9c9f3a265db316
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-05-31 00:02:28 +0200

    Trim new value for temperature converter

--- src/components/guis/temperature-converter/temperature-converter.helpers.ts
+++ src/components/guis/temperature-converter/temperature-converter.helpers.ts
@@ -4,5 +4,5 @@ export const toFahrenheit = (celsiusValue: number) =>
   celsiusValue * (9 / 5) + 32
 export const isStringValidNumber = (str: string) => {
   const num = Number(str)
-  return !(Number.isNaN(num) || str.length === 0)
+  return !(Number.isNaN(num) || str.trim().length === 0)
 }
