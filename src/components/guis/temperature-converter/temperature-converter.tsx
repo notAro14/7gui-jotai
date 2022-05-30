79db88577ae0eace8af889dfc302e5b1c11be3b1
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-05-30 21:13:10 +0200

    Implement html structure for temperature converter

--- src/components/guis/temperature-converter/temperature-converter.tsx
+++ src/components/guis/temperature-converter/temperature-converter.tsx
@@ -2,11 +2,34 @@ import { FC } from "react"
 
 import Flex from "src/shared/flex"
 import Heading from "src/shared/heading"
+import Label from "src/shared/label"
+import Input from "src/shared/input"
 
 const TemperatureConverter: FC = () => {
   return (
-    <Flex as="article">
+    <Flex
+      css={{
+        marginBottom: "$base",
+        border: "1px solid $hi-text",
+        borderRadius: "$md",
+        padding: "$base",
+      }}
+      as="article"
+      direction="column"
+      gap={5}
+    >
       <Heading as="h2">Temperature converter</Heading>
+      <Flex direction="column" gap={5}>
+        <Flex direction="column">
+          <Label htmlFor="celsius">Celsius</Label>
+          <Input id="celsius" />
+        </Flex>
+
+        <Flex direction="column">
+          <Label htmlFor="fahrenheit">Fahrenheit</Label>
+          <Input id="fahrenheit" />
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
